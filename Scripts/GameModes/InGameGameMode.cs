a7e1a334222860b9129ae76df3b4080740f142f2
Michael Christie <michaelchristie26@gmail.com>
2021-01-11 11:53:13 +0000

    Simple Player Force Movement

---
+++ Scripts/GameModes/InGameGameMode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameGameMode : MonoBehaviour
+{
+    private static InGameGameMode instance;
+    //public getter for the gamemode instance
+    public static InGameGameMode GetGameMode() { return instance; }
+
+    private void Awake()
+    {
+        //sets up the gamemode instance
+        if (instance)
+            Destroy(this);
+        instance = this;
+    }
+}
---
+++ Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //if the user touches the screen
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            //adds force
+            if (Application.isEditor)
+                AddUpwardsForce(3.0f);
+            else
+                AddUpwardsForce(.75f);
+        }
+    }
+
+    void AddUpwardsForce(float amount)
+    {
+        rb.AddForce(Vector3.up * amount, ForceMode.Impulse);
+    }
+}
