5556e4e709516c585d32f044e44123260e9c5f91
Michael Christie <michaelchristie26@gmail.com>
2021-01-11 13:33:38 +0000

    Main Menu UI

---
+++ Scripts/UI/MainMenuWidget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuWidget : MonoBehaviour
+{
+    [SerializeField]
+    Button btnPlayGame;
+    [SerializeField]
+    Text txtHighScore;
+
+    private void Awake()
+    {
+        //adds the play game to the onclick delegate
+        btnPlayGame.onClick.AddListener(PlayGame);
+        txtHighScore.text = "High Score:\n0000"; //change this to the new highscore
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
